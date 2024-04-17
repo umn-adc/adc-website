3ea1e51daf9f635b6cf6ab867210aaf1bbb132de
Victor Hofstetter <victorbarroshof@gmail.com>
2024-04-17 09:49:35 -0500

    switch to hash routing

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter } from 'react-router-dom';
 import Home from 'routes/Home';
 import Gallery from './Gallery';
 import Challenges from './Challenges';
 
-const router = createBrowserRouter([
+// ? GitHub hosting does not support anything but hash routing
+// TODO: figure out hosting to use BrowserRouter instead
+const router = createHashRouter([
   { path: '/', element: <Home /> },
   { path: '/gallery', element: <Gallery /> },
   { path: '/challenges', element: <Challenges /> },
